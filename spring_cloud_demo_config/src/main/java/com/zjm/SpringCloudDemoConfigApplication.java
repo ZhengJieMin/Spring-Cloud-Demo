0aacde9605149bf6d30f97873824b66496cbed38
zhengjiemin <zjm12345>
2018-05-31 10:54:36 +0800

    static

--- spring_cloud_demo_config/src/main/java/com/zjm/SpringCloudDemoConfigApplication.java
+++ spring_cloud_demo_config/src/main/java/com/zjm/SpringCloudDemoConfigApplication.java
@@ -1,11 +1,16 @@
 package com.zjm;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
 @EnableConfigServer
+@EnableAutoConfiguration
+@EnableDiscoveryClient
 public class SpringCloudDemoConfigApplication {
 
 	public static void main(String[] args) {
